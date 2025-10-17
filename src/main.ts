795ed51e5091756599bcb1d4f501c7de13d9ad3f
MD. ARIFUL ISLAM <arif171042@gmail.com>
2025-10-17 14:44:08 +0600

    Update main file

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,12 @@ async function bootstrap() {
     origin: [process.env.FRONTEND_URL ?? 'http://localhost:3000'],
     credentials: true,
   });
-  // app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 8001;
+
+  await app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${port}/api`);
+  });
 }
 bootstrap();
