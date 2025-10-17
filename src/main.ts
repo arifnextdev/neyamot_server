51fa7b62d013aebae982af6de176a921b96ade8e
MD. ARIFUL ISLAM <arif171042@gmail.com>
2025-10-17 18:37:58 +0600

    update API_URL

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL || 'http://localhost:3000',
+      process.env.API_URL || 'http://localhost:3000',
       'http://localhost:3000',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
